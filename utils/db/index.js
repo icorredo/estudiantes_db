8f982ce4ffa3febe013cf304dbd167178d55f039
Estudiantes RSEF <estudiantes@rsef.es>
2021-12-05 14:39:48 +0100

    [AF] Updated db variables to rsef-test1 ones

--- utils/db/index.js
+++ utils/db/index.js
@@ -2,7 +2,7 @@ import admin from 'firebase-admin'
 
 const serviceAccount = {
   type: 'service_account',
-  project_id: 'rsef-c0e6d',
+  project_id: 'rsef-test1',
   private_key_id: process.env.PRIVATE_KEY_ID,
   private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.CLIENT_EMAIL,
@@ -11,14 +11,14 @@ const serviceAccount = {
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
   client_x509_cert_url:
-    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zxp7c%40rsef-c0e6d.iam.gserviceaccount.com',
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-kjgok%40rsef-test1.iam.gserviceaccount.com',
 }
 
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-      databaseURL: 'https://rsef-c0e6d.firebaseio.com/',
+      databaseURL: 'https://rsef-test1.firebaseio.com/',
     })
   } catch (error) {
     console.log('Firebase admin initialization error', error.stack)
