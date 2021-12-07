a26c732d2e1705228d7e66ed720c7999e1aa33b2
Estudiantes RSEF <estudiantes@rsef.es>
2021-12-07 17:13:23 +0100

    [AF] 4th test to achieve multi-register when multi-check

--- pages/post.js
+++ pages/post.js
@@ -68,12 +68,12 @@ const Post = () => {
         </Heading>
         <FormControl id="cbox" isRequired mb={marginBetweenElements} as='fieldset'>
           <FormLabel as="legend">CheckboxGroup test</FormLabel>
-          <CheckboxGroup onChange={onChange} variant="filled">
+          <CheckboxGroup value={content.cbox} onChange={onChange} variant="filled">
             <HStack spacing="24px">
-              <Checkbox type="text" name="cbox1" value="Valor1">Valor1</Checkbox>
-              <Checkbox type="text" name="cbox2" value="Valor2">Valor2</Checkbox>
-              <Checkbox type="text" name="cbox3" value="Valor3">Valor3</Checkbox>
-              <Checkbox type="text" name="cbox4" value="Valor4">Valor4</Checkbox>
+              <Checkbox type="text" name="cbox1">Valor1</Checkbox>
+              <Checkbox type="text" name="cbox2">Valor2</Checkbox>
+              <Checkbox type="text" name="cbox3">Valor3</Checkbox>
+              <Checkbox type="text" name="cbox4">Valor4</Checkbox>
             </HStack>
           </CheckboxGroup>
         </FormControl>
